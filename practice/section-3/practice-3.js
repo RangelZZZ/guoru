4ce878367ca538477cafa062b7bf0e5797aaaf0f
RangelZZZ <18792561236@163.com>
2016-04-09 15:42:38 +0800

    perfect

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,68 +1,39 @@
-function create_updated_collection(collection_a, object_b) {
+function  create_updated_collection(collection_a, object_b) {
 
-  	var count = new Array();
+ var cNewArray = buildNewArray(collection_a);
 
-	for(var k = 0;k < 100; k++){
-		count[k] = 0;
-	}
-
-	var newArray = new Array();
-
-	for(var i = 0;i < collection_a.length;i++){
-		if(collection_a[i] === 'a'){
-			count[0]++;
-			newArray[0] = {"key": collection_a[i],"count":count[0]};
-			continue;
-		}
-		if(collection_a[i] === 'e'){
-			count[1]++;
-			newArray[1] = {"key": collection_a[i],"count":count[1]};
-			continue;
-		}
-		if(collection_a[i] === 'h'){
-			count[2]++;
-			newArray[2] = {"key": collection_a[i],"count":count[2]};
-			continue;
-		}
-		if(collection_a[i] === 't'){
-			count[3]++;
-			newArray[3] = {"key": collection_a[i],"count":count[3]};
-			continue;
-		}
-		if(collection_a[i] === 'f'){
-			count[4]++;
-			newArray[4] = {"key": collection_a[i],"count":count[4]};
-			continue;
-		}
-		if(collection_a[i] === 'c'){
-			count[5]++;
-			newArray[5] = {"key": collection_a[i],"count":count[5]};
-			continue;
-		}
-		if(collection_a[i] === 'g'){
-			count[6]++;
-			newArray[6] = {"key": collection_a[i],"count":count[6]};
-			continue;
-		}
-		if(collection_a[i] === 'b'){
-			count[7]++;
-			newArray[7] = {"key": collection_a[i],"count":count[7]};
-			continue;
-		}
-		if(collection_a[i] === 'd'){
-			count[8]++;
-			newArray[8] = {"key": collection_a[i],"count":count[8]};
-			continue;
-		}
-	}
-	
-      for (var m = 0; m < newArray.length; m++) {
-             for(var j = 0; j < object_b.value.length; j++){
-                 if (newArray[m].key === object_b.value[j]){
-                     newArray[m].count = newArray[m].count - parseInt(newArray[m].count/3);
-    }	
+  for(var i = 0;i < cNewArray.length;i++){
+    var element = cNewArray[i];
+    if(IsExit(element,object_b)){
+      element.count = element.count - parseInt(element.count/3);
+      cNewArray[i].count = element.count;
+    }
   }
-  }
-	return newArray;
+  return  cNewArray;
 }
 
+
+function IsExit(element,object_b){
+  for(var i = 0; i < object_b.value.length; i++){
+    if(element.key === object_b.value[i]){
+      return true;
+    }
+  }
+  return false;
+}
+
+
+function buildNewArray(collection_a){
+  var newArray = [];
+  var elementCount = 1;
+  for (var i = 0; i < collection_a.length; i++) {
+    if (collection_a[i] === collection_a[i + 1]) {
+      elementCount++;
+    }
+    else {
+      newArray.push({key: collection_a[i], count: elementCount});
+      elementCount = 1;
+    }
+  }
+  return newArray;
+}
