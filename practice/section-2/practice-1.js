84f237af76567f70d920e0b27c739376633d7a3c
RangelZZZ <18792561236@163.com>
2016-04-09 15:34:59 +0800

    perfect

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,61 +1,17 @@
-function count_same_elements(collection) {
+function  count_same_elements(collection){
 
-	var count = new Array();
+  var elementCount =  1;
+  var newArray = [];
+  for(var i = 0;i < collection.length;i++){
+    if(collection[i] === collection[i+1]){
+              elementCount++;
+    }
+    else{
+              newArray.push({key:collection[i],count:elementCount});
+              elementCount = 1;
+    }
+  }
+  return newArray;
 
-	for(var i = 0; i < 100; i++){
-			count[i] = 0;
-	}
+  }
 
-	var newArray = new Array();
-
-	for(var i = 0;i < collection.length;i++){
-		if(collection[i] === 'a'){
-			count[0]++;
-			newArray[0] = {"key":collection[i],"count":count[0]};
-			continue;
-		}
-		if(collection[i] === 'e'){
-			count[1]++;
-			newArray[1] = {"key":collection[i],"count":count[1]};
-			continue;
-		}
-		if(collection[i] === 'h'){
-			count[2]++;
-			newArray[2] = {"key":collection[i],"count":count[2]};
-			continue;
-		}
-		if(collection[i] === 't'){
-			count[3]++;
-			newArray[3] = {"key":collection[i],"count":count[3]};
-			continue;
-		}
-		if(collection[i] === 'f'){
-			count[4]++;
-			newArray[4] = {"key":collection[i],"count":count[4]};
-			continue;
-		}
-		if(collection[i] === 'c'){
-			count[5]++;
-			newArray[5] = {"key":collection[i],"count":count[5]};
-			continue;
-		}
-		if(collection[i] === 'g'){
-			count[6]++;
-			newArray[6] = {"key":collection[i],"count":count[6]};
-			continue;
-		}
-		if(collection[i] === 'b'){
-			count[7]++;
-			newArray[7] = {"key":collection[i],"count":count[7]};
-			continue;
-		}
-		if(collection[i] === 'd'){
-			count[8]++;
-			newArray[8] = {"key":collection[i],"count":count[8]};
-			continue;
-		}
-	}
-
-	
-	return newArray;
-}
